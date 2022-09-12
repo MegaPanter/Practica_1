3b5a88d22dcb88af7fd6ad76f844eddf11ce6bfe
MegaPanter <113056259+MegaPanter@users.noreply.github.com>
2022-09-11 21:45:37 -0500

    Update blinky.c

--- blinky.c
+++ blinky.c
@@ -1,95 +1,49 @@
-
-#include "lib/include.h"
-
-unsigned long Led;
-
-void Delay(void){unsigned long volatile time;
-  time = 800000;
-  while(time){
-        time--;
-  }
-}
-
-int main() {
-
+int
+main(void)
+{
+    volatile uint32_t valor;
     volatile uint32_t ui32Loop;
-                        //operacione con bitwise
-                        // N      J       F     B
-    SYSCTL->RCGCGPIO |= (1<<12)|(1<<8)|(1<<5)|(1<<1);
-    SYSCTL->RCGCGPIO |= 0x1122;
-    while((SYSCTL->PRGPIO&0x00000100) == 0){;} // allow time for clock to stabilize
-    ui32Loop = SYSCTL->RCGCGPIO;
-    //Salidas
-    GPIOB_AHB->DIR |= (1<<4);
-    GPIOF_AHB->DIR |= (1<<4) | (1<<0);
-        GPION->DIR |= (1<<1) | (1<<0);
-    //Entradas
-    GPIOJ_AHB->DIR |= (0<<1)|(0<<0);
-    GPIOJ_AHB->PUR  |= (1<<1)|(1<<0);//Estado alto
-    //Funciones alternativas
-    GPIOB_AHB->AFSEL |= 0x00;
-    GPIOJ_AHB->AFSEL |= 0x00;
-        GPION->AFSEL |= 0x00;
-    GPIOF_AHB->AFSEL |= 0x00;
-    //Tabla con GPIOCTL
-    GPIOB_AHB->PCTL |= 0x00;
-    GPIOJ_AHB->PCTL |= 0x00;
-        GPION->PCTL |= 0x00;
-    GPIOF_AHB->PCTL |= 0x00;
-    //Habilitarlos como digitales
-    GPIOB_AHB->DEN |= (1<<4);
-    GPIOJ_AHB->DEN |= (1<<1) | (1<<0);
-        GPION->DEN |= (1<<1) | (1<<0);
-    GPIOF_AHB->DEN |= (1<<4) | (1<<0);
-    //GPION->DIR  = 0x03; 
-    //GPION->DEN  = 0x03;
 
-  /*Super micros*/
-/*esto es una actualizacion*/
+    //
+    // establecemos los puertos que abriremos en este caso los n,f y m
+    //
+    SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R12|SYSCTL_RCGCGPIO_R5|SYSCTL_RCGCGPIO_R11;
 
-  while (1U)
-  {
-        //
-        // Turn on the LED.
-        //
-         GPION->DATA |= 0x01;
-        //GPIO_PORTN_DATA_R |= 0x01;
+   
+    ui32Loop = SYSCTL_RCGCGPIO_R;
 
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 2000000; ui32Loop++)
+   
+
+    GPIO_PORTN_DIR_R = 0x01; //seleccionamos el puerto n numero 0
+    GPIO_PORTN_DEN_R = 0x01;
+    GPIO_PORTF_AHB_DIR_R = 0x02; //seleccionamos el puerto F numero 1
+    GPIO_PORTF_AHB_DEN_R = 0x02;
+    GPIO_PORTM_DIR_R = 0x02; //seleccionamos el puerto m numero 1
+     GPIO_PORTM_DEN_R = 0x02;
+
+//creacion del loop infinito
+
+    while(1)
+    {
+       O_PORTF_AHB_DATA_R |= 0x02; //activamos el puerto f
+         GPIO_PORTN_DATA_R &= ~(0x01); //apagamos el puerto n
+         GPIO_PORTM_DATA_R &= ~(0x02); //apagamos el puerto m
+      
+        for(ui32Loop = 0; ui32Loop < valor; ui32Loop++) // creamos un loop de x microsegundos 
+        //definido por el valor que asociemos a la varible valor
         {
         }
-         GPION->DATA |= 0x02;
-        //GPIO_PORTN_DATA_R |= 0x01;
 
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 2000000; ui32Loop++)
+        
+         GPIO_PORTN_DATA_R |= 0x01; //encendemos el puerto n
+         GPIO_PORTF_AHB_DATA_R &= ~(0x02); //apagamos el puerto f
+         GPIO_PORTM_DATA_R |= 0x02; //encendemos el puero m
+
+ 
+        for(ui32Loop = 0; ui32Loop < valor; ui32Loop++)  //creamos un loop entre la operacion contraria a la pasada para 
+   //que mientras unos esten encendidos los otros esten apagados
         {
         }
-        //
-        // Turn off the LED.
-        //
-        GPION->DATA &= ~(0x01);
-        //GPIO_PORTN_DATA_R &= ~(0x01);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-                GPION->DATA &= ~(0x02);
-        //GPIO_PORTN_DATA_R &= ~(0x01);
-
-        //
-        // Delay for a bit.
-        //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
-  }
+        
+    }
 }
